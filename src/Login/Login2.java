2a7f0f86faef68bfe9700e0f8751a0a1da11fd1c
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2019-05-14 12:59:24 +0200

    Create Login2.java

---
+++ src/Login/Login2.java
@@ -0,0 +1,13 @@
+package Login;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login2 extends Application {
+	public void start(Stage primaryStage) {
+		VBox vpane = new VBox()
+	}
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
